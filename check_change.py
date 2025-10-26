fd807c9eba23c12177302341479c4cb4305fa235
BlockHaity <104670904+BlockHaity@users.noreply.github.com>
2025-10-26 11:23:19 +0800

    添加自动同步发版工作流

---
+++ check_change.py
@@ -0,0 +1,21 @@
+from urllib.request import urlopen
+import json
+
+data = json.loads(urlopen("https://api.github.com/repos/BlueArchiveArisHelper/BAAH/releases/latest").read())
+tag_name = data["tag_name"]
+body = data["body"]
+
+with open("version.json", "r") as f:
+    context = json.loads(f.read())
+
+if tag_name == context["tag_name"]:
+    print("No change")
+    exit(1)
+
+context["version"] = str(tag_name)
+context["context"] = str(body)
+
+with open("version.json", "w") as f:
+    f.write(json.dumps(context))
+
+exit(0)
